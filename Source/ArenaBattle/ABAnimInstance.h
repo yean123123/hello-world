fb694c15b1d68c9aad2df4b39639d6eddfa1ea9d
yean123123 <54879918+yean123123@users.noreply.github.com>
2019-10-23 15:57:48 +0900

    10/23

---
+++ Source/ArenaBattle/ABAnimInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UABAnimInstance();
+	virtual void NativeUpdateAnimation(float DaltaSeconds) override;
+
+	void PlayAttackMontage();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta=(AllowPrivateAccess = true))
+	float CurrentPawnSpeed;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool IsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		UAnimMontage* AttackMontage;
+};
